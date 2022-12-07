d61f06197aadd348110241073bef0b7b1b17e6f1
BT_3W2_23 <isaack36@nycstudents.net>
2022-12-07 12:14:12 -0500

    commit

--- src/Game.java
+++ src/Game.java
@@ -1,40 +1,45 @@
 public class Game {
     private String answer;
 
+    private int lives;
     private String lvl;
     private String guesses;
-    private int lives;
 
     public Game() {
+        lvl = "normal";
         lives = 6;
         guesses = "";
         int r = (int) (Math.random() * 10);
         if (r == 9) {
-            answer = "crab";
-        } else if (r == 8) {
             answer = "banana";
+        } else if (r == 8) {
+            answer = "computer";
         } else if (r == 7) {
-            answer = "tarantula";
+            answer = "science";
         } else if (r == 6) {
-            answer = "quintillion";
+            answer = "bald";
         } else if (r == 5) {
-            answer = "parallelization";
+            answer = "camel";
         } else if (r == 4) {
-            answer = "myrrh";
+            answer = "mammoth";
         } else if (r == 3) {
-            answer = "squirreled";
+            answer = "robin";
         } else if (r == 2) {
-            answer = "traumatize";
+            answer = "cancer";
         } else if (r == 1) {
-            answer = "expedite";
+            answer = "calculus";
         } else if (r == 0) {
-            answer = "eel";
+            answer = "apple";
         }
     }
 
+    /**
+     *
+     * @param lvl represents the difficulty level of the game
+     */
     public Game(String lvl) {
-        lives = 6;
         guesses = "";
+        lives = 6;
         int r = (int) (Math.random() * 10);
         if (lvl.equals("hard")) {
             if (r == 9) {
@@ -66,13 +71,13 @@ public class Game {
             } else if (r == 7) {
                 answer = "science";
             } else if (r == 6) {
-                answer = "bald";
+                answer = "call";
             } else if (r == 5) {
                 answer = "camel";
             } else if (r == 4) {
                 answer = "mammoth";
             } else if (r == 3) {
-                answer = "robin";
+                answer = "orange";
             } else if (r == 2) {
                 answer = "cancer";
             } else if (r == 1) {
@@ -83,18 +88,35 @@ public class Game {
         }
     }
 
+    /**
+     * The method adds a letter to the list of letters the user has guessed
+     * @param str represents a letter the user guessed.
+     */
     public void addGuess(String str) {
         guesses += str;
     }
 
+    /**
+     *
+     * @return The method returns the letters the user has guessed.
+     */
     public String getGuesses() {
         return guesses;
     }
 
+    /**
+     *
+     * @return The method returns the difficulty level of the game
+     */
     public String getLvl() {
         return lvl;
     }
 
+
+    /**
+     *
+     * @return The method returns the letters the user guessed that are not in the word
+     */
     public String getWrongGuesses() {
         String guessList = "";
         for (int i = 0; i < guesses.length(); i++) {
@@ -108,13 +130,25 @@ public class Game {
         return guessList;
     }
 
+    /**
+     *
+     * @return The method returns the number of lives the user has left
+     */
     public int getLives() {
         return lives;
     }
 
+    /**
+     * The method substracts 1 from the variable lives
+     */
     public void loseLife() {
         lives--;
     }
+    /**
+     *
+     * @param ltr represents a letter that the user guessed
+     * @return The method returns true if parameter ltr is in the instance variable answer
+     */
     public boolean inWord(String ltr) {
         if (answer.contains(ltr)) {
             return true;
@@ -122,6 +156,10 @@ public class Game {
         return false;
     }
 
+    /**
+     * @param guessedWord represents the all the letters the user has guessed
+     * The method prints the letters that the user guessed in the form of a list
+     */
     public void printGuessedLetters(String guessedWord) {
         for (int i = 0; i < answer.length(); i++) {
             if (!guessedWord.contains(answer.substring(i, i + 1))) {
@@ -132,6 +170,11 @@ public class Game {
         }
     }
 
+    /**
+     *
+     * @param guessedWord represents all the letters the user guessed.
+     * @return The method returns true if the user has guessed all the letters in the answer
+     */
     public boolean win(String guessedWord) {
         for (int i = 0; i < answer.length(); i++) {
             if (!guessedWord.contains(answer.substring(i, i + 1))) {
@@ -141,18 +184,34 @@ public class Game {
         return true;
     }
 
+    /**
+     * The method prints the game
+     */
     public void printGame() {
         System.out.print("|--------------------|\n| ");
         printGuessedLetters(guesses);
         for (int i = answer.length(); i < 19; i++) {
             System.out.print(" ");
         }
-        System.out.println("|         Wrong Guesses: " + getWrongGuesses());
+        System.out.println("|           Wrong Guesses: " + getWrongGuesses());
         System.out.println("|                    |");
         System.out.println("| You have " + getLives() + " lives   |");
         System.out.println("|--------------------|");
         System.out.print("Pick a letter: ");
-        System.out.println();
+    }
 
+    /**
+     * The method prints the game at the end
+     */
+    public void printGameEnd() {
+        System.out.print("|--------------------|\n| ");
+        printGuessedLetters(guesses);
+        for (int i = answer.length(); i < 19; i++) {
+            System.out.print(" ");
+        }
+        System.out.println("|           Wrong Guesses: " + getWrongGuesses());
+        System.out.println("|                    |");
+        System.out.println("|                    |");
+        System.out.println("|--------------------|");
     }
 }
--- src/GameRunner.java
+++ src/GameRunner.java
@@ -3,33 +3,42 @@ import java.util.Scanner;
 public class GameRunner {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("\nH   A   N   G   M   A   N\n");
-        boolean retry = true;
+
         Game word = new Game();
-        while (retry == true) {
-            System.out.print("The difficulty is set to " + word.getLvl() + ". ");
-            if (scan.nextLine().equals("y")) {
-                word = new Game("hard");
-            }
+        while (true) {
+            System.out.println("The difficulty is set to " + word.getLvl());
             while (!word.win(word.getGuesses()) && word.getLives() > 0) {
                 word.printGame();
                 String guessedLetter = scan.nextLine();
-                word.addGuess(guessedLetter);
-                if (!(word.inWord(guessedLetter))) {
-                    word.loseLife();
+                if (guessedLetter.length() == 1) {
+                    word.addGuess(guessedLetter);
+                    if (!(word.inWord(guessedLetter))) {
+                        word.loseLife();
+                    }
+                } else {
+                    System.out.println("pick A LETTER");
                 }
+
             }
+            word.printGameEnd();
+
             if ((word.getLives() != 0)) {
                 System.out.println("You Win!");
             } else {
                 System.out.println("You Lose");
             }
-            word.printGame();
-            System.out.print("Press y to play again: ");
+
+            System.out.print("\nPress y to play again: ");
             if (!(scan.nextLine().equals("y"))) {
-                retry = false;
+                break;
             }
+
+            System.out.print("Set difficulty(normal or hard): ");
+            word = new Game(scan.nextLine());
         }
+        System.out.println("Thanks for playing!");
 
     }
 }
